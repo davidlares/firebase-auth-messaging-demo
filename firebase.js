64d41c36f0362046b9f612b643a08597ad12a31c
David Lares <david.e.lares@gmail.com>
2018-04-23 08:36:34 -0500

    Firebase credentials Update

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 var config = {
   apiKey: "XXX_XXXXXX_XXXXX",
   authDomain: "XXX_XXXXXX_XXXXX.firebaseapp.com",
-  databaseURL: "https://firstapp-b6c83.firebaseio.com",
+  databaseURL: "https://XXX_XXXXXX_XXXXX.firebaseio.com",
   projectId: "XXX_XXXXXX_XXXXX",
   storageBucket: "XXX_XXXXXX_XXXXX.appspot.com",
   messagingSenderId: "XXX_XXXXXX_XXXXX"
